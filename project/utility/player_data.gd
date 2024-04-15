60f8aacd33e806c415ec9f1b6eb6481a120c5ea0
WilsonVinson <cprewilsonvinson@gmail.com>
2024-04-15 15:37:45 +0800

    支付系统上线

--- project/utility/player_data.gd
+++ project/utility/player_data.gd
@@ -4,5 +4,13 @@ extends Resource
 class_name MoYuPlayerData
 ## [MoYu] 玩家数据存储资源
 
-@export var money : int = 0
-@export var food : int = 0
+@export var price : Dictionary = {
+	"money":0, # 金币
+	"food":0, # 食物
+	"wood":0, # 木材
+	"stone":0, # 岩石
+	"sand":0, # 沙子
+	"metal":0, # 金属
+	"quartz":0, # 石英
+}
+
--- project/utility/tile_data.gd
+++ project/utility/tile_data.gd
@@ -11,11 +11,11 @@ class_name MoYuTileData
 @export var price : Dictionary = {
 	"money":0, # 金币
 	"food":0, # 食物
-	# "wood":0, # 木材
-	# "stone":0, # 岩石
-	# "sand":0, # 沙子
-	# "metal":0, # 金属
-	# "quartz":0, # 石英
+	"wood":0, # 木材
+	"stone":0, # 岩石
+	"sand":0, # 沙子
+	"metal":0, # 金属
+	"quartz":0, # 石英
 }
 
 @export var can_build_on : Array
