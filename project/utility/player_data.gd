813ea574e1cd889cd57865f15c1cbfc81624e031
WilsonVinson <cprewilsonvinson@gmail.com>
2024-04-15 15:09:40 +0800

    瓷砖管理更新

--- project/utility/player_data.gd
+++ project/utility/player_data.gd
@@ -5,5 +5,4 @@ class_name MoYuPlayerData
 ## [MoYu] 玩家数据存储资源
 
 @export var money : int = 0
-
 @export var food : int = 0
--- project/utility/tile_data.gd
+++ project/utility/tile_data.gd
@@ -4,8 +4,18 @@ extends Resource
 class_name MoYuTileData
 ## [MoYu] 瓷砖数据存储资源
 
-@export var tile_id : int
-@export var tile_name : String
+@export var tile_id : int = 0
+@export var tile_name : String = "air"
 @export var tile_texture : Texture2D
+
+@export var price : Dictionary = {
+	"money":0, # 金币
+	"food":0, # 食物
+	# "wood":0, # 木材
+	# "stone":0, # 岩石
+	# "sand":0, # 沙子
+	# "metal":0, # 金属
+	# "quartz":0, # 石英
+}
 
 @export var can_build_on : Array
