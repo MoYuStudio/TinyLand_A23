67486d698b0316f0516e58fff93177039c435347
WilsonVinson <cprewilsonvinson@gmail.com>
2024-04-22 15:03:13 +0800

    数据结构更新

--- project/utility/player_data.gd
+++ project/utility/player_data.gd
@@ -5,14 +5,77 @@ class_name MoYuPlayerData
 ## [MoYu] 玩家数据存储资源
 
 @export var price : Dictionary = {
-	"money":0, # 金币
+	# 货币类
+	"shell":0, # 贝壳
+	"copper_coin":0, # 铜币
+	"iron_coin":0, # 铁币
+	"silver_coin":0, # 银币
+	"gold_coin":0, # 金币
+	"titanium_coin":0, # 钛币
+	
+	# 人口 人力 电力
+	"population":0, # 人口
 	"manpower":0, # 人力
-	"food":0, # 食物
-	"wood":0, # 木材
-	"stone":0, # 岩石
-	"quar":0, # 砂岩
-	"metal":0, # 金属
-	"glass":0, # 玻璃
+	"electricity":0, # 电力
+	
+	# 食物
+	"berries":0, # 浆果
+	"wheat":0, # 小麦
+	"corn":0, # 玉米
+	"rice":0, # 水稻
+	"canned_food":0, # 罐头食品
+	"cultured_meat":0, # 培养肉
+	"frozen_cultured_meat":0, # 冷冻培养肉
+	
+	# 冰 水
 	"ice":0, # 冰块
+	"water":0, # 水
+	
+	# 基础资源
+	"dirt":0, # 泥土
+	"clay":0, # 黏土
+	"wood":0, # 木材
+	"hardwood":0, # 硬木材
+	"rock":0, # 岩石
+	"sand":0, # 沙子
+	
+	# 加工物
+	"dirt_plank":0, # 土板
+	"wood_plank":0, # 木板
+	"hardwood_plank":0, # 硬木板
+	"rock_plank":0, # 石板
+	"glass":0, # 玻璃
+	
+	# 原矿
+	"copper_ore":0, # 铜矿石
+	"tin_ore":0, # 锡矿石
+	"aluminum_ore":0, # 铝矿石
+	"iron_ore":0, # 铁矿石
+	"silver_ore":0, # 银矿石
+	"gold_ore":0, # 金矿石
+	"titanium_ore":0, # 钛矿石
+	"lithium_ore":0, # 锂矿石
+	"uranium_ore":0, # 铀矿石
+	"silicon_ore":0, # 硅矿石
+	
+	# 矿物
+	"coal":0, # 煤碳块
+	"copper_ingot":0, # 铜锭
+	"tin_ingot":0, # 锡锭
+	"aluminum_ingot":0, # 铝锭
+	"iron_ingot":0, # 铁锭
+	"silver_ingot":0, # 银锭
+	"gold_ingot":0, # 金锭
+	"titanium_ingot":0, # 钛锭
+	"lithium_ingot":0, # 锂锭
+	"uranium_ingot":0, # 铀锭
+	"silicon_ingot":0, # 硅锭
+	
+	# 合金
+	"bronze_alloy":0, # 青铜合金
+	"aluminum_alloy":0, # 铝合金
+	"titanium_alloy":0, # 钛合金
+	"silicon_wafer":0, # 硅片
+	
 }
 
--- project/utility/tile_data.gd
+++ project/utility/tile_data.gd
@@ -8,16 +8,6 @@ class_name MoYuTileData
 @export var tile_name : String = "air"
 @export var tile_texture : Texture2D
 
-@export var price : Dictionary = {
-	"money":0, # 金币
-	"manpower":0, # 人力
-	"food":0, # 食物
-	"wood":0, # 木材
-	"stone":0, # 岩石
-	"quar":0, # 砂岩
-	"metal":0, # 金属
-	"glass":0, # 玻璃
-	"ice":0, # 冰块
-}
+@export var price : Dictionary = {}
 
 @export var can_build_on : Array
