ba375e089a02e1f067eb8ba31b611162d9900954
WilsonVinson <cprewilsonvinson@gmail.com>
2024-04-24 11:27:34 +0800

    TilesBar结构更新

--- project/utility/tile_data.gd
+++ project/utility/tile_data.gd
@@ -5,9 +5,15 @@ class_name MoYuTileData
 ## [MoYu] 瓷砖数据存储资源
 
 @export var tile_id : int = 0
-@export var tile_name : String = "air"
+# @export var tile_name : String = "air"
 @export var tile_texture : Texture2D
 
 @export var price : Dictionary = {}
 
+@export var manufacture : Dictionary = {}
+
 @export var can_build_on : Array
+
+func _ready():
+	print(self.get_name())
+	pass
