b46183a5d2878b2c5fc3dcf3a9c160293d351b86
WilsonVinson <cprewilsonvinson@gmail.com>
2024-05-08 10:19:24 +0800

    更新MoYuTileData类属性 修复层级问题

--- project/utility/tile_data.gd
+++ project/utility/tile_data.gd
@@ -4,16 +4,22 @@ extends Resource
 class_name MoYuTileData
 ## [MoYu] 瓷砖数据存储资源
 
+# 瓷砖索引
 @export var tile_id : int = 0
-# @export var tile_name : String = "air"
+# 瓷砖名称 使用self.get_name()获取文件名
+# 瓷砖材质&图标
 @export var tile_texture : Texture2D
 
+# 造价
 @export var price : Dictionary = {}
-
+# 生产
 @export var manufacture : Dictionary = {}
 
-@export var can_build_on : Array
+# 建造层级
+@export var building_level : int
+# 可被建造表面
+@export var building_built_on : Array
 
 func _ready():
-	print(self.get_name())
+	# print(self.get_name())
 	pass
